9244aa39ed2132be55ae53b25a458120277c0539
rachel-wong <hello.rachelwong@gmail.com>
2026-05-21 07:49:51 +1000

    feat: added header, establishd how to import styling and structure basicomponents in vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
-<script setup lang="ts"></script>
+<script setup>
+import Header from '@/components/Header.vue'
+</script>
 
 <template>
+  <Header/>
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header">
+    <div class="header__wrapper">
+      <img :src="logo" class="header__wrapper__logo" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import logo from '@/assets/images/brand-wordmark-onbrand.svg?url'
+
+</script>
+
+<style lang="scss">
+  @use '@/styles/components/Header.scss';
+</style>
